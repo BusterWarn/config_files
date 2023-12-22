9223f5de8a863f54beb86a8c7d5bdc1a56b157c2
ehulbus <buster.hultgren.warn@ericsson.com>
2023-12-22 08:53:14 +0100

    Attached clangd-extension to LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,4 +7,18 @@ return {
       "elixir-ls",
     },
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "p00f/clangd_extensions.nvim",
+      init = function()
+        require("lazyvim.util").lsp.on_attach(function(_, buffer)
+          require("clangd_extensions.inlay_hints").setup_autocmd()
+          require("clangd_extensions.inlay_hints").set_inlay_hints()
+        end)
+      end,
+    },
+  },
+
 }
